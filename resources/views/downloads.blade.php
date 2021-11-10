938afa8583c64fa0c926d52ee808e8c6bb22f2cd
Mark Sutherland <mark@marksuth.dev>
2021-11-10 17:33:41 +0000

    Remove old download instructions

--- resources/views/downloads.blade.php
+++ resources/views/downloads.blade.php
@@ -38,13 +38,8 @@
                 <li class="nav-item"><a class="nav-link" href="https://github.com/marksuth/indigenous-desktop/releases/download/v1.3.0/indigenous_1.3.0_amd64.deb"><i class="fab fa-fw fa-linux"></i> Linux Installer (.deb)</a></li>
                 <li class="nav-item"><a class="nav-link" href="https://github.com/marksuth/indigenous-desktop/releases/download/v1.3.0/indigenous-1.3.0-1.x86_64.rpm"><i class="fab fa-fw fa-linux"></i> Linux Installer (.rpm)</a></li>
             </ul>
-            <h3>Installation Notes</h3>
-            <h4>Linux</h4>
-            <p>Unzip && cd Indigenous-linux-x64 && ./Indigenous</p>
-            <h4>Windows</h4>
-            <p>Unzip, go in to the folder and double click 'Indigenous'. You will have to 'Right Click' on 'Indigenous' -> 'Properties' and toggle 'Unblock' the first time.</p>
-            <h4>macOS</h4>
-            <p>unzip and double click the app. You will likely have to open it with 'Right Click' -> 'Open' the first time.</p>
+            <p>For additional .zip builds, please see the <a href="https://github.com/marksuth/indigenous-desktop/releases/tag/v1.3.0">release page on GitHub</a></p>
+
         </div>
 
     </div>
